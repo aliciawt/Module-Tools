3e570c7e0d4683a4a885125ef6685b4609d9402d
Alicia Wynona Tjahjadi <alicia@intranet.rosadelsvents.es>
2026-04-01 14:23:28 +0200

    Answer ls-grep

--- shell-pipelines/ls-grep/script-04.sh
+++ shell-pipelines/ls-grep/script-04.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -euo pipefail
-
+ls sample-files | grep '^[A-Z][^A-Z]*$' | wc -l
 # TODO: Write a command to count the number of files in the sample-files directory whose name starts with an upper case letter and doesn't contain any other upper case letters.
 # Your output should be the number 7.
