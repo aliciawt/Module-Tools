1c65b6ff950b3d459d4531e8054740a15e8a89df
Alicia Wynona Tjahjadi <alicia@intranet.rosadelsvents.es>
2026-04-27 23:36:54 +0200

    Exercise 4 change add to date of birth

--- sprint-5-prep-exercises/exercise-4.py
+++ sprint-5-prep-exercises/exercise-4.py
@@ -1,19 +1,25 @@
+import datetime
+
 class Person:
-    def __init__(self, name: str, age: int, preferred_operating_system: str):
+    def __init__(self, name: str, date_of_birth: datetime.date, preferred_operating_system: str):
         self.name = name
-        self.age = age
+        self.date_of_birth = date_of_birth
         self.preferred_operating_system = preferred_operating_system
 
-imran = Person("Imran", 22, "Ubuntu")
+imran = Person("Imran", datetime.date(2004, 5, 15), "Ubuntu")
 print(imran.name)
 print(imran.address) # Error: "Person" has no attribute "address"
 
-eliza = Person("Eliza", 34, "Arch Linux")
+eliza = Person("Eliza", datetime.date(1992, 4, 26), "Arch Linux")
 print(eliza.name)
 print(eliza.address) # Error: "Person" has no attribute "address"
 
 def is_adult(person: Person) -> bool:
-    return person.age >= 18
+    today = datetime.date.today()
+    age = today.year - person.date_of_birth.year
+    if (today.month, today.day) < (person.date_of_birth.month, person.date_of_birth.day):
+        age -= 1
+    return age >= 18
 
 print(is_adult(imran))
 
